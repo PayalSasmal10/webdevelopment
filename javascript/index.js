980c954d7d8f89f4c492f52756bd14a7b2397ba8
Payal Sasmal <sasmalpayal@gmail.com>
2022-06-11 17:46:24 +0530

    commiting new javascript code

--- javascript/index.js
+++ javascript/index.js
@@ -2,3 +2,11 @@
 var tweet = prompt("compose your tweet:");
 var tweetCount = tweet.length;
 alert("You have written " + tweetCount + " characters, you have " + (140-tweetCount) + " characters left.");
+
+// slice operator
+
+
+var tweet = prompt("compose your tweet:");
+var tweetCount = tweet.slice(0,140);
+alert(tweetCount);
+
