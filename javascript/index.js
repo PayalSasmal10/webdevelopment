1f61bbae6d2eaab20e9a9b41c799f48c2efe6b97
Payal Sasmal <sasmalpayal@gmail.com>
2022-06-12 22:30:07 +0530

    learnt about upper case and lower case in js

--- javascript/index.js
+++ javascript/index.js
@@ -10,3 +10,16 @@ var tweet = prompt("compose your tweet:");
 var tweetCount = tweet.slice(0,140);
 alert(tweetCount);
 
+
+// changing case Text
+
+var myName = "Payal";
+myName.toUpperCase();
+
+var name = prompt("what is your name");
+
+var first_ch = name.slice(0,1);
+
+var remain_ch = name.slice(1);
+
+alert("Hello, " + first_ch.toUpperCase() + remain_ch.toLowerCase())
