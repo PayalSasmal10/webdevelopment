bb74b9e851fdae69c4a2b6ca3abda416db2819d0
Payal Sasmal <sasmalpayal@gmail.com>
2022-06-13 17:15:38 +0530

    commited bmi calculator

--- javascript/index.js
+++ javascript/index.js
@@ -23,3 +23,29 @@ var first_ch = name.slice(0,1);
 var remain_ch = name.slice(1);
 
 alert("Hello, " + first_ch.toUpperCase() + remain_ch.toLowerCase())
+
+
+// bmi calculator
+
+function bmiCalculator (weight, height) {
+    var bmi = weight / (height * height);
+    console.log(bmi)
+    var interpretation = "";
+    if (bmi < 18.5) {
+        // alert("Your BMI is " + interpretation + ", so you are underweight.")
+        interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+        console.log("interpretation :" + interpretation)
+    }
+    else if(bmi > 18.5 && bmi < 24.9) {
+        // alert("Your BMI is " + interpretation + ", so you a normal weight.")
+        interpretation = "Your BMI is " + bmi + ", so you a normal weight.";
+    }
+    else {
+        // alert("Your BMI is " + interpretation + ", so you are overweight.")
+        interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+    }
+    return interpretation;
+}
+
+var bmiMessage = bmiCalculator(60,2)
+console.log(bmiMessage)
