c2ad88800a606b74109ab879d1ef472b07952a01
Payal Sasmal <sasmalpayal@gmail.com>
2022-06-18 23:11:39 +0530

    drum kits completed

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -6,7 +6,8 @@ for (var i=0; i < numberOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         var buttonInnerHTML = this.innerHTML;
-        makeSound(buttonInnerHTML);        
+        makeSound(buttonInnerHTML);  
+        buttonAnimation(buttonInnerHTML);      
 
     }); 
 
@@ -17,6 +18,7 @@ for (var i=0; i < numberOfDrumButtons; i++) {
 
 document.addEventListener("keydown", function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 
 });
                                                                                                                                                                                     
@@ -61,4 +63,14 @@ function makeSound(key){
             console.log(buttonInnerHTML);
     }
 
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+ currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
