300f5ead8364a6a630c5de5c04246782317f4a0e
Payal Sasmal <sasmalpayal@gmail.com>
2022-06-18 21:29:43 +0530

    drum kits done

---
+++ Drum Kit/index.js
@@ -0,0 +1,7 @@
+
+document.querySelector(".set").addEventListener("click", function handleClick(){
+    
+});                                                                                                                                                                                      
+
+//var audio = new Audio("sounds/tom-1.mp3");
+//audio.play();
