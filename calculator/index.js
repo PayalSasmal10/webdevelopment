bfd414c016d876df9938015f520a3518cc20bb6d
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-01 18:56:12 +0530

    calculator input field all values are coming

--- calculator/index.js
+++ calculator/index.js
@@ -8,11 +8,16 @@ let outputInputField = 0;
 for (var i =0; i< numberofButtton; i++){
     document.querySelectorAll(".btn")[i].addEventListener("click", function(){
         var buttonInnerHTML = this.innerHTML;
-        if (outputInputField != 0){
+        if (outputInputField != 0 && buttonInnerHTML != '+' && buttonInnerHTML != '-' && buttonInnerHTML != '/' && buttonInnerHTML != '*'){
             outputInputField += buttonInnerHTML;
             document.getElementsByName('text_input')[0].value=parseInt(outputInputField);
             console.log(outputInputField);
-        }else{
+        }else if(outputInputField != 0 && buttonInnerHTML === '+'){
+            console.log("checking + value", buttonInnerHTML);
+            document.getElementsByName('text_input')[0].value='+';
+            // console.log(outputInputField);
+        }
+        else{
             outputInputField = document.getElementsByName('text_input')[0].value=buttonInnerHTML;
         }
            
