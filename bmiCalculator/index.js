fedafb9f4d9c78f27eb6e9ba33f806063c1dc014
Payal Sasmal <sasmalpayal@gmail.com>
2022-08-16 12:01:47 +0530

    bmi calculator

--- bmiCalculator/index.js
+++ bmiCalculator/index.js
@@ -1,9 +1,23 @@
-const plus = document.getElementById("pluss");
-console.log(plus);
+let plusbtn = document.getElementById("pluss");
+let minusbtn = document.getElementById("minuss");
+
+
+console.log(minusbtn);
+console.log(plusbtn);
 let val = parseInt(document.querySelector('input').value);
 function btn_click(){
-    val += 1;
-    document.querySelector("input").value = val;
+    if (document.getElementById('pluss').clicked === true){
+        console.log('minus'+ minusbtn.textContent);
+        val = val - 1;
+        document.querySelector("input").value = val;
+    }
+    else if (plusbtn.textContent === '+' ) {
+        console.log('plus'+ plusbtn.textContent);
+        val += 1;
+        document.querySelector("input").value = val;
+    }
+    
+    
     
 }
 
