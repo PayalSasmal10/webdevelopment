84d2f4bda184d404d280794b7041fcbac3225ecc
Payal Sasmal <sasmalpayal@gmail.com>
2022-08-17 12:25:48 +0530

    used addevenlistenser to listen the span element

--- bmiCalculator/index.js
+++ bmiCalculator/index.js
@@ -1,24 +1,25 @@
 let plusbtn = document.getElementById("pluss");
 let minusbtn = document.getElementById("minuss");
 
-
-console.log(minusbtn);
-console.log(plusbtn);
-let val = parseInt(document.querySelector('input').value);
-function btn_click(){
-    if (document.getElementById('pluss').clicked === true){
-        console.log('minus'+ minusbtn.textContent);
-        val = val - 1;
-        document.querySelector("input").value = val;
-    }
-    else if (plusbtn.textContent === '+' ) {
-        console.log('plus'+ plusbtn.textContent);
-        val += 1;
-        document.querySelector("input").value = val;
-    }
-    
-    
-    
+let spans = document.querySelectorAll('.span_btn').length;
+console.log(spans);
+for(var i = 0; i<spans; i++){
+    document.querySelectorAll('.span_btn')[i].addEventListener("click", function(){
+        var spanInnerHtml = this.innerHTML;
+        pressSpanBtn(spanInnerHtml);
+    } );
 }
 
+function pressSpanBtn(key){
+    let val = parseInt(document.querySelector('input').value);
+    switch(key){
+        case "-":
+            val -= 1;
+            document.querySelector("input").value = val;
+            break;
+        case "+":
+            val += 1;
+            document.querySelector("input").value = val;
+    }
+}
 
