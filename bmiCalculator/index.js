43f66c277d16c2a4fe1d0e9b38af93d2871857bc
Payal Sasmal <sasmalpayal@gmail.com>
2022-08-17 12:26:18 +0530

    used addevenlistenser to listen the span element

--- bmiCalculator/index.js
+++ bmiCalculator/index.js
@@ -1,8 +1,6 @@
-let plusbtn = document.getElementById("pluss");
-let minusbtn = document.getElementById("minuss");
 
 let spans = document.querySelectorAll('.span_btn').length;
-console.log(spans);
+
 for(var i = 0; i<spans; i++){
     document.querySelectorAll('.span_btn')[i].addEventListener("click", function(){
         var spanInnerHtml = this.innerHTML;
