2d60cb14de765e0041be5f08378040c4d99cb0f6
Payal Sasmal <sasmalpayal@gmail.com>
2022-08-22 20:56:05 +0530

    created arrow sign

--- bmiCalculator/index.js
+++ bmiCalculator/index.js
@@ -1,4 +1,4 @@
-
+// set the kgs and height values.
 let spans = document.querySelectorAll('.span_btn').length;
 
 for(var i = 0; i<spans; i++){
@@ -18,6 +18,21 @@ function pressSpanBtn(key){
         case "+":
             val += 1;
             document.querySelector(".count").value = val;
+            break;
     }
 }
 
+// change the icon color
+// let icons = document.querySelectorAll('.icon').length;
+// console.log(icons);
+// for(var i = 0; i< icons; i++){
+//     document.querySelectorAll('.icon')[i].addEventListener("click", function(){
+//         var iconHtml = this.innerHTML;
+//         console.log(iconHtml);
+//     })
+// }
+const iconColor = document.querySelectorAll("icon");
+console.log(iconColor);
+// iconColor.addEventListener("click", function onClick(){
+//     iconColor.style.color = 'blue';
+// })
