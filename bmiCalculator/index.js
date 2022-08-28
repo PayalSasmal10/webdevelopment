e1a13297b9cd4224f3ee94fcdf40917ff01bdf7e
Payal Sasmal <sasmalpayal@gmail.com>
2022-08-28 22:22:08 +0530

    result input value setting is completed

--- bmiCalculator/index.js
+++ bmiCalculator/index.js
@@ -58,4 +58,5 @@ document.querySelector(".btn-bmi").addEventListener("click", function(){
     let weight = document.querySelector(".count").valueAsNumber;
     let bmi = (weight/(heights/100)**2).toFixed(1); // round the number.
     console.log(bmi);
+    document.querySelector(".result").value = bmi;
 });
