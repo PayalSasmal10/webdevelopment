fd66cb17090575691e7ba44b0b376a86db0b5c65
Payal Sasmal <sasmalpayal@gmail.com>
2022-09-14 15:41:05 +0530

    change the background color after click on a button

--- change background color/index.js
+++ change background color/index.js
@@ -1,5 +1,12 @@
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const color = ["#D800A6", "#1CD6CE", "#8758FF", "#BFACE0", "#D9CB50"];
 
 
-document.querySelector(".click").addEventListener("click", function(){
-    document.body.style.background = "#367E18";
-})
+button.addEventListener("click", btn_click);
+
+function btn_click(){
+    const colorIndex = parseInt(Math.random()*color.length);
+    body.style.backgroundColor = color[colorIndex];
+}
+
