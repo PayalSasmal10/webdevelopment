11adf0be7f7ee04f0bd149dedb36b7c8f952de12
Payal Sasmal <sasmalpayal@gmail.com>
2022-09-15 22:23:31 +0530

    pass the message event listener is completed

---
+++ pass the message/index.js
@@ -0,0 +1,22 @@
+
+const form = document.querySelector("#message-form");
+console.log(form);
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const message = document.querySelector("#message");
+    const feedback = document.querySelector(".feedback");
+    const messageContent = document.querySelector(".message-content");
+    if(message.value === ''){
+        feedback.classList.add('show');
+        setTimeout(function(){
+            feedback.classList.remove('show')
+        }, 4000);
+    }
+    else{
+        messageContent.textContent = message.value;
+        message.value = '';
+    }
+
+})
+
