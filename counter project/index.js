52d4f0151911c8e8b1533ce416189ec6aca0b472
Payal Sasmal <sasmalpayal@gmail.com>
2022-09-19 17:26:32 +0530

    couter project is in progress, fix the auto refresg suing prevant default function

--- counter project/index.js
+++ counter project/index.js
@@ -1,12 +1,19 @@
 const count = document.querySelector("#counter");
-
-const buttons = document.querySelectorAll("#button").length;
+console.log(count.value);
+const buttons = document.querySelectorAll("#button");
 console.log(buttons);
 // console.log("test", document.querySelectorAll("#button"));
-for(let i= 0; i<buttons; i++){
+for(let i= 0; i<buttons.length; i++){
     
     document.querySelectorAll("#button")[i].addEventListener("click", function(e) {
         e.preventDefault();
-        
+        let counts = parseInt(count.innerHTML);
+        if(buttons[i].className === "down"){
+            counts -= 1;
+            
+        }else{
+            counts += 1;
+        }
+        count.innerHTML = counts
     })
 }
