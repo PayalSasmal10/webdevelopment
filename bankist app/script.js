faeafcdc8357cd55f8dd8c2cb951211cf41e597d
Payal Sasmal <sasmalpayal@gmail.com>
2022-09-26 13:28:48 +0530

    bankist app started

--- bankist app/script.js
+++ bankist app/script.js
@@ -61,16 +61,28 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+const displayMovements = function (movements){
+  movements.forEach(function(mov, i){
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    const html = ` <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+    <div class="movements__date">3 days ago</div>
+    <div class="movements__value">4 000€</div>
+  </div>`
+  })
+}
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
