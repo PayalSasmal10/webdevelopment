6a31a65b196c8eb26d0012468be7691ece3c8860
Payal Sasmal <sasmalpayal@gmail.com>
2022-10-01 12:01:13 +0530

    creating username from user's full name

--- bankist app/script.js
+++ bankist app/script.js
@@ -78,12 +78,14 @@ const displayMovements = function (movements){
 displayMovements(account3.movements);
 
 // const user = 'Steven Thomas Williams';
-const createUsernames = function(user){
-  const username = user.toLowerCase().split(' ').map(name => name[0]).join('');
-  return username;
-}
+const createUsernames = function(accs){
+  accs.forEach(function(acc){
+    acc.username = acc.owner.toLowerCase().split(' ').map(name => name[0]).join('');
+  });
+};
 
-console.log(createUsernames('Steven Thomas Williams'));
+createUsernames(accounts);
+console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
