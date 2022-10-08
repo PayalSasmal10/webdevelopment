e03edd024badcfff7d0d5d1f4e1dac2d732641f2
Payal Sasmal <sasmalpayal@gmail.com>
2022-10-08 17:00:28 +0530

    sorting array feature is completed

--- bankist app/script.js
+++ bankist app/script.js
@@ -62,8 +62,11 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 
-const displayMovements = function (movements){
-  movements.forEach(function(mov, i){
+const displayMovements = function (movements, sort = false){
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function(mov, i){
     const type = mov > 0 ? "deposit" : "withdrawal";
     const html = ` <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
@@ -185,6 +188,14 @@ btnLoan.addEventListener("click", function(e){
 
   }
   inputLoanAmount.value = "";
+});
+
+let sorted = false;
+
+btnSort.addEventListener("click", function(e){
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 })
 
 
@@ -200,7 +211,7 @@ btnClose.addEventListener("click", function(e){
     containerApp.style.opacity = 0;
   }
   inputCloseUsername.value = inputClosePin.value = '';
-})
+});
 
 
 /////////////////////////////////////////////////
