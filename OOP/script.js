7a9062a9eb402650da4963c4ea14ed77888bcb02
Payal Sasmal <sasmalpayal@gmail.com>
2022-10-14 16:23:06 +0530

    understanding prototype

--- OOP/script.js
+++ OOP/script.js
@@ -1,12 +1,13 @@
 'use strict';
 
+// Contructure
 const Person =  function(firstName, age) {
     this.firstName = firstName;
     this.age = age;
-}
+};
 
 console.log(new Person('Payal', 1994));
-
+const payal = new Person('Payal', 1994);
 // What happended when we create a contructure using- new Person()
 // 1. New {} (empty) object created.
 // 2. function is called, then this keyword set to be newly created empty object {}, i.e this = {}
@@ -16,3 +17,28 @@ console.log(new Person('Payal', 1994));
 const prince = new Person('Prince', 1996);
 
 console.log(prince);
+console.log(prince instanceof Person);
+
+
+// Prototypes
+Person.prototype.calcAge = function(){
+    console.log(2022 - this.age);
+};
+
+prince.calcAge();
+payal.calcAge();
+
+console.log(payal.__proto__);
+console.log(payal.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(payal));
+console.log(Person.prototype.isPrototypeOf(prince));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .LinkedObjectOfProtytpe
+
+Person.prototype.specices = 'Human';
+
+console.log(payal);
+console.log(prince);
+console.log(payal.hasOwnProperty('species'));
+console.log(payal.hasOwnProperty('age'));
