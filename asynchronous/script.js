b7ea0e18e1e6559462d296aaa18419f3292b4732
Payal Sasmal <sasmalpayal@gmail.com>
2022-10-19 16:28:48 +0530

    Promise creataion examples

--- asynchronous/script.js
+++ asynchronous/script.js
@@ -256,3 +256,19 @@ const lotteryPromise = new Promise(function (resolve, reject) {
 });
 
 lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// promisifying setTimeOut
+const wait = function (seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000);
+
+    });
+    
+};
+
+wait(2)
+    .then(() => {
+        console.log('I waited for 2 seconds....');
+        return wait(1);
+    })
+    .then(() => console.log('I waited for 1 second'));
