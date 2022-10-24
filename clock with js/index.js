62e8bbe7f9a0f1c8a34bc62488aa8fcdb918ada4
Payal Sasmal <sasmalpayal@gmail.com>
2022-10-24 22:24:03 +0530

    clock with js with mins and hour is also completed

--- clock with js/index.js
+++ clock with js/index.js
@@ -10,10 +10,12 @@ function setDate() {
     const secondDegrees = ((seconds /60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
+    // Mins hand
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + 90;
     minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
+    // Hour hand
     const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
