95dce0c5592884384efe5f5a2893fbf08b1da131
Payal Sasmal <sasmalpayal@gmail.com>
2022-10-26 13:36:46 +0530

    array cardio 1 2nd way

--- array cardio day 1/index.js
+++ array cardio day 1/index.js
@@ -23,11 +23,19 @@ const inventors = [
   
   // Array.prototype.filter()
   // 1. Filter the list of inventors for those who were born in the 1500's
+//   1st way
+/*
  const fifteen = inventors.filter(function (inventor) {
      if(inventor.year >= 1500 && inventor.year < 1600){
          return true;
      }
      
+ });*/
+ 
+ const fifteen = inventors.filter(inventor => {
+    if(inventor.year >= 1500 && inventor.year < 1600) {
+        return true;
+    }
  });
  console.table(fifteen);
 
