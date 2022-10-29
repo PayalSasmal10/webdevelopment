b01cf64b881159f31b19d8e674b56ee1efcf204f
Payal Sasmal <sasmalpayal@gmail.com>
2022-10-29 22:58:40 +0530

    javascript is work in progress

--- groceryList/index.js
+++ groceryList/index.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector(".add-items");
 const itemList = document.querySelector(".plates");
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
     e.preventDefault();
@@ -10,8 +10,34 @@ function addItem(e) {
         text : text,
         done: false
     }
-    console.log(item);
+    items.push(item);
+    populateList(items,itemList);
+    localStorage.setItem('items', JSON.stringify(items));
     this.reset();
 };
 
+function populateList(plates = [], itemList) {
+    itemList.innerHTML = plates.map((plate, i) => {
+        return `
+            <li>
+                <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''}>
+                <label for="item${i}">${plate.text}</label>
+            </li>
+        `
+    }).join('');
+    
+}
+
+function toggleDone(e) {
+    if(!e.target.matches('input')) return; // skips unless it is input.
+    const index = e.target.dataset.index;
+    console.log(index);
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemList);
+}
+
 addItems.addEventListener('submit', addItem);
+itemList.addEventListener('click', toggleDone);
+
+populateList(items, itemList);
